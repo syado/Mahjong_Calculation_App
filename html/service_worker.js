2a74a49f274d305b1ebc67a1bdb373e69dcfd522
to-omouzyan <ken03matsui07@gmail.com>
2019-05-30 14:18:24 +0900

    ふえた

---
+++ html/service_worker.js
@@ -0,0 +1,33 @@
+//キャッシュファイルの指定
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+    '/jokeplace.github.io/',
+];
+
+//インストール処理
+self.addEventListener('install',function(event)
+{
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache)
+            {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+/*
+//リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch',function(event)
+{
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(responce)
+            {
+                return responce ? responce :fetch(event.request);
+            })
+    );
+});
+*/
