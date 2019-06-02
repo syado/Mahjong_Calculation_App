a836467ce329ef2db7c2617d1256d572ef9bfb76
syado <doi.sinya122@gmail.com>
2019-06-02 18:43:25 +0900

    img_api 位置情報追加

--- api_server/view/img_api.py
+++ api_server/view/img_api.py
@@ -56,7 +56,7 @@ def img_post():
         file = request.files['image']
         img_path = os.path.join("./uploads", file.filename)
         file.save(img_path)
-        p = dl_post(img_path, 0.7)
+        p = dl_post_v2(img_path, 0.7)
         return jsonify(p) 
 
 def dl_get(img_path, ritu):
@@ -123,3 +123,45 @@ def dl_post(img_path, ritu):
                 display_txt = '{}'.format(label_name)
             return p
 
+def dl_post_v2(img_path, ritu):
+    inputs = []
+    images = []
+    img = image.load_img(img_path, target_size=(512, 512))
+    img = image.img_to_array(img)
+    images.append(imread(img_path))
+    inputs.append(img.copy())
+    inputs = preprocess_input(np.array(inputs)) 
+
+    with graph.as_default():  
+        preds = model.predict(inputs, batch_size=1, verbose=1)
+
+        results = bbox_util.detection_out(preds)
+
+        for i, img in enumerate(images):
+            det_label = results[i][:, 0]
+            det_conf = results[i][:, 1]
+            det_xmin = results[i][:, 2]
+            det_ymin = results[i][:, 3]
+            det_xmax = results[i][:, 4]
+            det_ymax = results[i][:, 5]
+
+            top_indices = [i for i, conf in enumerate(det_conf) if conf >= ritu]
+
+            top_conf = det_conf[top_indices]
+            top_label_indices = det_label[top_indices].tolist()
+            top_xmin = det_xmin[top_indices]
+            top_ymin = det_ymin[top_indices]
+            top_xmax = det_xmax[top_indices]
+            top_ymax = det_ymax[top_indices]
+            p = {}
+            for i in range(top_conf.shape[0]):
+                xmin = int(round(top_xmin[i] * img.shape[1]))
+                ymin = int(round(top_ymin[i] * img.shape[0]))
+                xmax = int(round(top_xmax[i] * img.shape[1]))
+                ymax = int(round(top_ymax[i] * img.shape[0]))
+                score = top_conf[i]
+                label = int(top_label_indices[i])
+                label_name = voc_classes[label - 1]
+                p[i]= {"name":label, "score":score, "position":{"x":xmin,"y":ymin,"h":ymax-ymin,"w":xmax-xmin}}
+                display_txt = '{}'.format(label_name)
+            return p
