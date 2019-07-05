42f2e668ecc543d512a435750631c4a750e48771
to-omouzyan <ken03matsui07@gmail.com>
2019-07-05 14:55:52 +0900

    index画面を調整 次api.js

--- web_site/api.js
+++ web_site/api.js
@@ -1,42 +1,40 @@
-//下記はカメラデータの送信部分
+// Web上カメラの送信
 function camera_send() {
     $( ".loading" ).fadeIn( "fast" ) ;
-    //送信先URL
+    // 送信先URL
     var host_url = "https://mahjong.syado.net/img_api/base64";
-    //canvas elementを取得
+    // canvas elementを取得
     var canvas = document.getElementById('canvas');
-    //base64データを取得（エンコード）
+    // base64データを取得（エンコード）
     var img_base64 = canvas.toDataURL('image/png');
-    
     var fd = new FormData();
     fd.append('img', img_base64);
 
     $.ajax({
-        //画像処理サーバーに返す場合
         url: host_url,
         type: 'POST',
         data: fd,
         contentType: false,
         processData: false,
-        success: function(data, dataType) {
+        success: function(data) {
             //非同期で通信成功時に読み出される [200 OK 時]
             console.log('Success', data);
-            seturl = "web_setting.html?"
-            //var ar = ["tehai_01", "tehai_02", "tehai_03", "tehai_04", "tehai_05", "tehai_06", "tehai_07", "tehai_08", "tehai_09", "tehai_10", "tehai_11", "tehai_12", "tehai_13", "agarihai"];
+            seturl = "web_setting.html?";
             for (var i = 0; i < 14; i++) {
                 if (data[i]){
-                    seturl += String(i)+"="+data[i]["name"]+"&"
+                    seturl += String(i) + "=" + data[i]["name"] + "&";
                 }
             }
             window.location.href = seturl.slice(0,-1);
         },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
+        error: function(errorThrown) {
             //非同期で通信失敗時に読み出される
             console.log('Error : ' + errorThrown);
         }
     });
 }
 
+// ファイル選択の送信
 function img_form_send() {
     $( ".loading" ).fadeIn( "fast" ) ;
     $.ajax({
@@ -49,7 +47,7 @@ function img_form_send() {
         complete: function(){
             loading_off();
         },
-        success: function (data, dataType) {
+        success: function (data) {
             //非同期で通信成功時に読み出される [200 OK 時]
             console.log('Success', data);
             seturl = "web_setting.html?"
@@ -60,7 +58,7 @@ function img_form_send() {
             }
             window.location.href = seturl.slice(0, -1);
         },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
+        error: function (errorThrown) {
             //非同期で通信失敗時に読み出される
             console.log('Error : ' + errorThrown);
         }
