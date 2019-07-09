6b6fd7fcc58490a5ef5dc24aa91174fee4786360
to-omouzyan <ken03matsui07@gmail.com>
2019-07-09 11:26:05 +0900

    カメラ周りの調整

--- web_site/modal_cmr.js
+++ web_site/modal_cmr.js
@@ -10,33 +10,32 @@ function centeringModalSyncer() {
 
 function modal_open() {
 
-	//キーボード操作などにより、オーバーレイが多重起動するのを防止する
-	$( this ).blur() ;	//ボタンからフォーカスを外す
-	if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない (防止策1)
-	//if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
+	// キーボード操作などにより、オーバーレイが多重起動するのを防止する
+	$( this ).blur() ;	// ボタンからフォーカスを外す
+	if( $( "#modal-overlay" )[0] ) return false ;  // 新しくモーダルウィンドウを起動しない
+	
 
-	//オーバーレイを出現させる
+	// オーバーレイを出現させる
 	$( "body" ).append( '<div id="modal-overlay"></div>' ) ;
 	$( "#modal-overlay" ).fadeIn( "fast" ) ;
 
-	//コンテンツをセンタリングする
+	// コンテンツをセンタリングする
 	centeringModalSyncer() ;
 
-	//コンテンツをフェードインする
+	// コンテンツをフェードインする
 	$( "#modal-content" ).fadeIn( "fast" ) ;
 
-	//[#modal-overlay]、または[#modal-close]をクリックしたら…
+	// [#modal-overlay]、または[#modal-close]をクリックしたら…
 	$( "#modal-overlay,#modal-close" ).unbind().click( function(){
+		// カメラを再起動
 		videoRestartbutton()
 
-		//[#modal-content]と[#modal-overlay]をフェードアウトした後に…
+		// [#modal-content]と[#modal-overlay]をフェードアウトした後に…
 		$( "#modal-content,#modal-overlay" ).fadeOut( "fast" , function(){
-
-			//[#modal-overlay]を削除する
+			
+			// [#modal-overlay]を削除する
 			$('#modal-overlay').remove() ;
-
 		} ) ;
-
 	} ) ;
 }
 
