3300f87ead612ee3c7e8c6d5b891ee5d03702c7e
to-omouzyan <ken03matsui07@gmail.com>
2019-07-09 13:36:59 +0900

    docs追加

---
+++ docs/service_worker.js
@@ -0,0 +1,28 @@
+// キャッシュファイルの指定
+var CACHE_NAME = 'Mahjong_Calculation_App';
+var urlsToCache = [
+    '/hai/',
+    '/image/',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
